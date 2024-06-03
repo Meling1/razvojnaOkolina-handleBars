d57e427e1e48d386df67382569ee1ad005041707
Martina <martinaeling99@gmail.com>
2024-06-03 18:37:55 +0200

    added zebra pattern

--- pokemon-script.js
+++ pokemon-script.js
@@ -17,10 +17,44 @@ function popuniPokemone() {
   document.getElementById("pokemon-output").innerHTML = html;
 }
 
-xhr.onload = function () {
-  popuniPokemone();
+function afterRender() {
+  $("table th").css("color", "darkBlue");
 
+  //drugi nacin dodavanja stila:
+  // $("table th").addClass("headerStyle");
+  // u tablici svaki drugi redak obojati drugom bojom
+  $("table tr:nth-child(even)").addClass("zebra");
+
+  // remove those starts with "p" after 2 sec
+  setTimeout(function () {
+    // find "a" starts with letter "p"
+    const hideMe = &("table td a:contains('p')").filter(function () {
+      // only element with first letter "p"
+      return this.innerHTML.indexOf("p") == 0;
+    });
+    // remove closest "tr" element
+    hideMe.closest("tr").remove();
+    // after removing some tr's our table is broken
+
+applyColorOnRow();
+  }, 2000)
+};
+
+function applyColorOnRow() {
+  // 1. remove all class zebra 
+  $("table tr:nth-child(even)").addClass("zebra");
+  //2. apply again class zebra on even row
+  $("table tr:nth-child")
+}
+
+
+xhr.onload = function () {
+  // napravi tablicu pokemona
+  popuniPokemone();
+  // aktiviraj popover
   $('[data-bs-toggle="popover"]').popover();
+  // zebra uzorak
+  afterRender();
 };
 
 xhr.send();
